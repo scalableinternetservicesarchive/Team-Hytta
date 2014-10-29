9d929aaa515b0a63d82d1ac32ca662b0344c3e33
tkskow <tkskow@gmail.com>
2014-10-28 19:49:25 -0700

    worked with bootstrap, and created button for navigation between cabin and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :cabin
+  #validates :title, presence: true
+  #validates :cabin, presence: true
 end
--- teamHytta/app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :cabin
-  belongs_to :parent
-end
